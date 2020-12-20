6f1182ede3d530dc440deefb139a88d14396d7b0
Carlos Kruger <carloskr@yahoo.com>
2020-12-20 12:08:19 -0500

    typo in add_trans

--- db/add_transaction.sql
+++ db/add_transaction.sql
@@ -1,14 +1,3 @@
-CREATE TABLE transactions (
-    transaction_id SERIAL PRIMARY KEY,
-    transaction_amount INT NOT NULL,
-    transaction_category VARCHAR(100) NOT NULL,
-    transaction_date DATE DEFAULT CURRENT_DATE,
-    iso_currency_code VARCHAR(50) NOT NULL,
-    pending BOOLEAN NOT NULL,
-    transaction_title VARCHAR(250) NOT NULL,
-    transaction_desc VARCHAR(500),
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE
-);
 
 INSERT INTO transactions (
       transaction_amount,
@@ -18,6 +7,6 @@ INSERT INTO transactions (
     pending,
     transaction_title,
     transaction_desc,
-    user_id I
+    user_id
 ) 
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
