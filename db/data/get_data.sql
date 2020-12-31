bfa4bdd84e5920742b4b4f508e09a9a03d7c0b1d
Jabinator1 <jabinator1@gmail.com>
2020-12-30 17:19:14 -0700

    working on get_data.sql

--- db/data/get_data.sql
+++ db/data/get_data.sql
@@ -1,4 +1,11 @@
-DO $$
-BEGIN
-    EXECUTE 'SELECT * FROM' || $2 || 'WHERE user_id =' || $1;
-END $$;
+CREATE OR REPLACE FUNCTION get_data(user_id INT, table_name anyelement)
+RETURNS SETOF ANYELEMENT AS $$
+    BEGIN
+        RETURN QUERY EXECUTE 
+            format('SELECT * FROM %s WHERE user_id = $1', pg_typeof(table_name)) USING user_id;
+    END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM get_data($1, NULL::$2);
+
+-- $1 is 5 and $2 is 'goals' for example
--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -1,11 +1,13 @@
+//# getData is currently not working
 const getData = async (req, res) => {
     const db = req.app.get('db')
     const { tableName } = req.params
     // const { userId } = req.session.user
-    const userId = 1
+    const userId = 5
 
-    const data = await db.data.get_data([userId, tableName])
+    const data = await db.data.get_data(userId, tableName)
     .catch(err => {console.log(err); res.status(500).send(err)})
+    console.log(data)
 
     res.status(200).send(data)
 }
@@ -14,7 +16,7 @@ const addData = async (req, res) => {
     const db = req.app.get('db')
     const { tableName } = req.params
     // const { userId } = req.session.user
-    const userId = 1
+    const userId = 5
 
     await db.data.add_data([userId, tableName, req.body])
     .catch(err => {console.log(err); res.status(500).send(err)})
@@ -26,19 +28,19 @@ const editData = async (req, res) => {
     const db = req.app.get('db')
     const { tableName, dataId } = req.params
     // const { userId } = req.session.user
-    const userId = 1
+    const userId = 5
 
-    const test = await db.data.edit_data([userId, tableName, req.body, dataId])
+    await db.data.edit_data([userId, tableName, req.body, dataId])
     .catch(err => {console.log(err); res.status(500).send(err)})
 
-    res.status(200).send(test)
+    res.sendStatus(200)
 }
 
 const deleteData = async (req, res) => {
     const db = req.app.get('db')
     const { tableName, dataId } = req.params
     // const { userId } = req.session.user
-    const userId = 1
+    const userId = 5
 
     await db.data.delete_data([userId, tableName, dataId])
     .catch(err => {console.log(err); res.status(500).send(err)})
