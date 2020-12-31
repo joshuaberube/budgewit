c293d2cdb2e544752b9c899ee2699a95722982db
Brian Lindeboom <bklindeboom@gmail.com>
2020-12-31 13:08:56 -0500

    controller framework

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,7 @@
-import {compareSync, genSaltSync, hashSync} from 'bcrypt'
+import {compareSync, genSaltSync, hashSync} from 'bcrypt';
+import nodemailer from 'nodemailer';
+
+
 
 const registerUser = async (req, res) => {
     const db = req.app.get('db')
@@ -49,4 +52,58 @@ const getUserSession = async (req, res) => {
     : res.status(404).send("Please login")
 }
 
-export {registerUser, loginUser, logoutUser, getUserSession}
+//import environment variables nodemailer
+const { authEmailer, authEmailerPassword } = process.env
+
+const email = async (req, res) => {
+    const { firstName, lastName, email, } = req.body  //user variables from the front end  
+    //icebox add conditional mailer for verify user.
+
+    try {
+      //invoke the createTransport function passing in your email information. 
+      let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: authEmailer,
+          pass: authEmailerPassword
+        }
+      });
+
+      //invoke the sendMail function with the info in the email
+      let resetPasswordEmail = await transporter.sendMail({
+        from: `'The Budgewit Team' <${authEmailer}>`, //From field with our name.
+        to: email, //user's email address
+        subject: 'Your Budgewit Account', //This will show on the subject of the email
+        text: `Hello ${firstName} ${lastName},
+        We see you are having some trouble with your account.
+        Use the link below to reset it.`, //for clients with plaintext support only
+        html: `<div>${message}<div>`, 
+              //<img src="cid:budgewit@gmail.com"/>`,  if we want to include a logo or attachments later.
+        /*attachments: [
+          { //this is the attachment of the document
+            filename: '',
+            path: ''
+          },
+          { //this is the embedded image
+            cid: 'budgewit@gmail.com', 
+            path:''
+          }
+        ]*/
+      }, (err, res) => {
+        if (err) {
+          console.log('err', err)
+        } else {
+          console.log('res', res)
+          res.status(200).send(resetPasswordEmail)
+        }
+      })
+    } catch (err) {
+      console.log(err)
+      res.sendStatus(500)
+    }
+
+    
+  }
+
+
+export {registerUser, loginUser, logoutUser, getUserSession, email}
