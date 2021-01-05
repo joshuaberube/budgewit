5c032ba90dd8c7f4570b20abf9a3e677b1bc9938
Brian Lindeboom <bklindeboom@gmail.com>
2021-01-05 17:11:14 -0500

    tweak

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <nav>
                 <NavLink to="/">Overview</NavLink>
                 <NavLink to="/transactions">Transactions</NavLink>
-                <NavLink to="/goals">Goals</NavLink>  //*this piece is combined with the Calculations piece*//
+                <NavLink to="/goals">Goals</NavLink>  
                 <NavLink to="/bills">Bills</NavLink>
                 <NavLink to="/budget">Budget</NavLink>
                 <NavLink to="/resources">Resources</NavLink>
