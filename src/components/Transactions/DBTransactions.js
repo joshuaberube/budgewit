6f9b15a8defe8981f4247fce293b44a2755c1273
Carlos Kruger <carloskr@yahoo.com>
2021-01-06 11:56:52 -0500

    restoring changes

---
+++ src/components/Transactions/DBTransactions.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Transactions = () => {
+	const [dbTransactions, setDBTransactions] = useState([]);
+
+	useEffect(async () => {
+		try {
+			const response = await axios.get("/api/data/transactions");
+			setDBTransactions(response.data);
+			console.log(response.data);
+		} catch (err) {}
+	});
+	return <div></div>;
+};
+
+export default Transactions;
