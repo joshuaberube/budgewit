45c5e3b1d4bc95800dc9d4b226516f7e18756137
Brian Lindeboom <bklindeboom@gmail.com>
2021-01-06 12:22:05 -0500

    fixed put bug

--- src/components/Resources/AddResource.js
+++ src/components/Resources/AddResource.js
@@ -2,84 +2,84 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const AddResources = () => {
-	const [resourceState, setResourceState] = useState({
-        title: resource.resource_title,
-        description: resource.resource_desc,
-        source: resource.resource_link,
-        category: resource.resource_category,
+  const [resourceState, setResourceState] = useState({
+    title: resource.resource_title,
+    description: resource.resource_desc,
+    source: resource.resource_link,
+    category: resource.resource_category,
+  });
+  const handleButton = (e) => {
+    e.preventDefault();
+    try {
+      let response = axios.post("/api/data/resources", {});
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	const handleSubmit = (evt) => {
-		evt.preventDefault();
-		try {
-			let response = axios.post("/api/data/resource_links", {
-				
-			});
-			console.log(response);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				Amount:
-				<input
-					type="number"
-					value={transaction_amount}
-					onChange={(e) => setAmount(e.target.value)}
-				/>
-			</label>
-			<label>
-				Category:
-				<input
-					type="text"
-					value={transaction_category}
-					onChange={(e) => setCategory(e.target.value)}
-				/>
-			</label>
-			<label>
-				Date:
-				<input
-					type="date"
-					value={transaction_date}
-					onChange={(e) => setDate(e.target.value)}
-				/>
-			</label>
-			<label>
-				Currency Code:
-				<input
-					type="text"
-					value={iso_currency_code}
-					onChange={(e) => setCurrencyCode(e.target.value)}
-				/>
-			</label>
-			<label>
-				Status (pending):
-				<select>
-					<option value="true">true</option>
-					<option value={pending}>false</option>
-					onChange={(e) => setPending(e.target.value)}
-				</select>
-			</label>
-			<label>
-				Transaction Title:
-				<input
-					type="text"
-					value={transaction_title}
-					onChange={(e) => setTitle(e.target.value)}
-				/>
-			</label>
-			<label>
-				Transaction Description:
-				<input
-					type="text"
-					value={transaction_desc}
-					onChange={(e) => setDescription(e.target.value)}
-				/>
-			</label>
-			<input type="submit" value="Submit" />
-		</form>
-	);
+  return (
+    <form>
+      <label>
+        Title:
+        <input
+          type="text"
+          value={resourceState.title}
+          onChange={(e) => {
+            const state = resourceState;
+            setResourceState({
+              ...state,
+              title: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Source:
+        <input
+          type="text"
+          value={resourceState.source}
+          onChange={(e) => {
+            const state = resourceState;
+            setResourceState({
+              ...state,
+              source: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Description:
+        <input
+          type="text"
+          value={resourceState.description}
+          onChange={(e) => {
+            const state = resourceState;
+            setResourceState({
+              ...state,
+              description: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Category:
+        <input
+          type="text"
+          value={resourceState.category}
+          onChange={(e) => {
+            const state = resourceState;
+            setResourceState({
+              ...state,
+              category: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <button type="submit" name="submitbutton" onClick={handleButton}>
+        Add a Link
+      </button>
+    </form>
+  );
 };
 export default AddResources;
