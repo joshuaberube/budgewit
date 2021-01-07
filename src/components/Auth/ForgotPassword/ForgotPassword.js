b5d5e130c4b72b11c8c5b15a5897aaba227127a4
Brian Lindeboom <bklindeboom@gmail.com>
2021-01-07 14:56:10 -0500

    forgot password

--- src/components/Auth/ForgotPassword/ForgotPassword.js
+++ src/components/Auth/ForgotPassword/ForgotPassword.js
@@ -27,26 +27,34 @@ const ForgotPassword = () => {
   return (
     <div className="w-768 bg-gray-300 rounded-10 flex flex-col shadow-2xl font-proxima-nova">
       <div className="mx-80 py-48">
-      <div className="flex flex-row justify-between items-baseline">
-        <h1 className="text-3xl text-gray-600 font-extrabold">Forgot Your Password?</h1>
+        <div className="flex flex-row justify-between items-baseline">
+          <h1 className="text-3xl text-gray-600 font-extrabold">
+            Forgot Your Password?
+          </h1>
+          <Link to="/auth">
+          <button className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer">
+            Return to Login
+          </button>
+        </Link>
+        </div>
+        <form className="flex flex-row border-t border-gray-400 pt-16 mt-2">
+          <input
+            className="rounded-5 h-40 w-256 p-12 text-sm placeholder-gray-400 text-gray-800 bg-gray-50 font-semibold tracking-wide"
+            type="text"
+            name="email"
+            placeholder="Insert email address."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button
+            className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer"
+            onClick={sendEmail}
+            type="submit"
+          >
+            Reset Password
+          </button>
+        </form>
       </div>
-      </div>
-      <form className="flex flex-row border-t border-gray-400 pt-16 mt-2">
-        <input
-          className="rounded-5 h-40 w-256 p-12 text-sm placeholder-gray-400 text-gray-800 bg-gray-50 font-semibold tracking-wide"
-          type="text"
-          name="email"
-          placeholder="Insert email address."
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer" onClick={sendEmail} type="submit">
-          Reset Password
-        </button>
-      </form>
-      <Link to="/auth">
-        <button className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer">Return to Login</button>
-      </Link>
     </div>
   );
 };
--- src/components/Auth/ResetPassword/ResetPassword.js
+++ src/components/Auth/ResetPassword/ResetPassword.js
@@ -6,14 +6,11 @@ const ResetPassword = () => {
   const [passState, setPassState] = useState({ password: null, match: null });
   const [passwordsMatch, setPasswordsMatch] = useState(false); //if true reset password button will render
   const [resetState, setResetState] = useState(false);
-  const resetPasswordToken = useParams()
-  
-  
-  
-  
+  const resetPasswordToken = useParams();
+
   const handleButton = async (e) => {
     e.preventDefault();
- 
+
     const { password } = passState;
     try {
       const response = await axios.put("/api/user/reset/:resetPasswordToken", {
@@ -39,40 +36,59 @@ const ResetPassword = () => {
   return (
     <>
       {!resetState ? (
-        <>
-          <h1>Password Reset Form</h1>
-          <form className="">
-            <input
-              type="password"
-              name="password"
-              placeholder="Insert your new password"
-              onChange={(e) =>
-                setPassState({ ...passState, password: e.target.value })
-              }
-            />
-            <input
-              type="password"
-              name="passwordmatch"
-              placeholder="Type in your password again"
-              onChange={(e) =>
-                setPassState({ ...passState, match: e.target.value })
-              }
-            />
-            {passwordsMatch ? (
-              <button type="submit" onClick={handleButton}>
-                Reset Password
+        <div className="w-768 bg-gray-300 rounded-10 flex flex-col shadow-2xl font-proxima-nova">
+          <div className="mx-80 py-48">
+            <div className="flex flex-row justify-between items-baseline">
+              <h1>Password Reset Form</h1>
+            </div>
+
+            <form className="">
+              <input
+                className="rounded-5 mb-16 h-40 w-256 p-12 text-sm placeholder-gray-400 text-gray-800 bg-gray-50 font-semibold tracking-wide"
+                type="password"
+                name="password"
+                placeholder="Insert your new password"
+                onChange={(e) =>
+                  setPassState({ ...passState, password: e.target.value })
+                }
+              />
+              <input
+                className="rounded-5 mb-16 h-40 w-256 p-12 text-sm placeholder-gray-400 text-gray-800 bg-gray-50 font-semibold tracking-wide"
+                type="password"
+                name="passwordmatch"
+                placeholder="Type in your password again"
+                onChange={(e) =>
+                  setPassState({ ...passState, match: e.target.value })
+                }
+              />
+              {passwordsMatch ? (
+                <button
+                  className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer"
+                  type="submit"
+                  onClick={handleButton}
+                >
+                  Reset Password
+                </button>
+              ) : (
+                <p>Passwords must match.</p>
+              )}
+            </form>
+            <Link to="/auth">
+              <button className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer">
+                Return to Login
               </button>
-            ) : (
-              <p>Passwords must match.</p>
-            )}
-          </form>
-          <Link to="/auth">Return to Login</Link>
-        </>
+            </Link>
+          </div>
+        </div>
       ) : (
         <>
           <h1>Your password has been reset!</h1>
 
-          <Link to="/auth">Return to Login</Link>
+          <Link to="/auth">
+            <button className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer">
+              Return to Login
+            </button>
+          </Link>
         </>
       )}
     </>
