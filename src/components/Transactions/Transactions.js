73a87fd36f3c58054a78cb3ff9bf0bca30be039c
Jabinator1 <jabinator1@gmail.com>
2021-01-07 16:53:32 -0700

    fixed bugs in App.js and reformated some

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,26 +1,37 @@
 import { useSelector } from "react-redux";
 import {useState} from 'react'
 import { transactionsFilteredSelector } from "../../redux/slices/plaidSlice";
-import AddTransactions from "./AddTransactions";
+import AddTransactions from "./AddTransactions/AddTransactions";
 import ChangeAccount from "./ChangeAccount/ChangeAccount";
 import TransactionRowItem from "./TransactionRowItem";
 
 const Transactions = () => {
-  const transactions = useSelector(transactionsFilteredSelector);
-  const [isAddTransaction, setIsAddTransaction] = useState(false);
-  const transactionsMapped = transactions.map((transaction) => (
+  const transactions = useSelector(transactionsFilteredSelector)
+  const [isAddTransaction, setIsAddTransaction] = useState(false)
+
+  const transactionsMapped = transactions.map(transaction => (
     <TransactionRowItem
       key={transaction.transaction_id}
       transaction={transaction}
     />
-  ));
-
-
-    const transactionsMapped = transactions.map(transaction => <TransactionRowItem key={transaction.transaction_id} transaction={transaction} />)
+  ))
 
     return (
         <div className="bg-gray-200">
             <ChangeAccount />
+            <div>
+                <input
+                    type="button"
+                    value="Create Transaction"
+                    onClick={() => setIsAddTransaction(!isAddTransaction)}
+                    className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer"
+                />
+                <div className="">
+                    {isAddTransaction ? (
+                        <AddTransactions setIsAddTransaction={setIsAddTransaction} />
+                    ) : null}
+                </div>                            
+            </div>
             <div className="w-768 bg-gray-300 rounded-10 shadow-3xl mx-auto mb-48">
                 <div className="mx-8 px-32 py-8 flex justify-between font-bold text-gray-500">
                     <span>Name</span>
@@ -32,19 +43,9 @@ const Transactions = () => {
                 <ul className="bg-gray-50 rounded-b-10">
                     {transactionsMapped}
                 </ul>
-                    <div>
-        <input
-          type="button"
-          value="Create Transaction"
-          onClick={() => setIsAddTransaction(!isAddTransaction)}
-          className="py-8 px-12 rounded-10 bg-green-400 text-gray-50 cursor-pointer"
-        />
-        <div className="">
-          {isAddTransaction ? (
-            <AddTransactions setIsAddTransaction={setIsAddTransaction} />
-          ) : null}
-
-        </div>                            
             </div>
+        </div>
+    )
+}
                                                  
-export default Transactions;
+export default Transactions
