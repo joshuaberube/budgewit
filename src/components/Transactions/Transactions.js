a3ecdf99d9367aa11069ff14577f67b052ccd14e
Jabinator1 <jabinator1@gmail.com>
2021-01-07 16:00:59 -0700

    finished transaction Row UI

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -9,10 +9,17 @@ const Transactions = () => {
     const transactionsMapped = transactions.map(transaction => <TransactionRowItem key={transaction.transaction_id} transaction={transaction} />)
 
     return (
-        <div className="bg-gray-50">
+        <div className="bg-gray-200">
             <ChangeAccount />
-            <div className="w-768 bg-gray-300 p-12 rounded-10">
-                <ul className="bg-gray-50 rounded-10">
+            <div className="w-768 bg-gray-300 rounded-10 shadow-3xl mx-auto mb-48">
+                <div className="mx-8 px-32 py-8 flex justify-between font-bold text-gray-500">
+                    <span>Name</span>
+                    <div className="flex flex-row">
+                        <div className="w-128 text-center">Type</div>
+                        <div className="w-128 text-right">Amount</div>
+                    </div>
+                </div>
+                <ul className="bg-gray-50 rounded-b-10">
                     {transactionsMapped}
                 </ul>
             </div>
